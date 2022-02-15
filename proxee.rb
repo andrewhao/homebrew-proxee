66d87cfbc4ee27cce36b65804850d24285a47887
Andrew Hao <andrewhao@gmail.com>
2022-02-15 14:01:45 -0800

    Adding linux bin

--- proxee.rb
+++ proxee.rb
@@ -4,9 +4,13 @@ class Proxee < Formula
   version "0.1.0"
 
   on_macos do
-    url "https://github.com/andrewhao/proxee/releases/download/v#{version}/darwin_amd64.tar.gz"
     url "https://github.com/andrewhao/proxee/releases/download/v#{version}/proxee_v#{version}_x86_64-apple-darwin.zip"
     sha256 "49f69fe625b0338e7780ef6dc666e0d91bf37f5f4f7d2426a283870522a20bcb"
+  end
+
+  on_linux do
+    url "https://github.com/andrewhao/proxee/releases/download/v#{version}/proxee_v#{version}_x86_64-unknown-linux-musl.zip"
+    sha256 "6eeaec8cace1ded7d545af9eeaead097904a8d2ff9f801a44698ccc32623bac2"
   end
 
   def install
